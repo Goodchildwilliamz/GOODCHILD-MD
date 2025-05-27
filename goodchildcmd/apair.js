e5ff7776551c64239dc42bbd61dba57f76468d00
DavincsTech <allandavinc89@gmail.com>
2025-05-27 10:06:11 +0300

    Update apair.js

--- goodchildcmd/apair.js
+++ goodchildcmd/apair.js
@@ -18,7 +18,7 @@ ezra({
     if (!_0x8621a4 || _0x8621a4.length === 0) {
       return _0x2e61d5("Example Usage: .code 255763755xxxx.");
     }
-    await _0x2e61d5("*Wait lucky-md is generating your pair code ✅...*");
+    await _0x2e61d5("*Wait Goodchild-md is generating your pair code ✅...*");
     const _0x386b0a = encodeURIComponent(_0x8621a4.join(" "));
     const _0x1ea92d = "https://goodchild.onrender.com/code?number=" + _0x386b0a;
     const _0xb59e41 = await axios.get(_0x1ea92d);
